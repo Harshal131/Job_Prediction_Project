b3f541692c2451a862dbfdb7169fd15dbe838bfd
Harshal Vakharia <vakharia.harshal18.hv@gmail.com>
2020-04-28 16:18:23 -0400

    Scrapper and code

---
+++ Collecting_data.py
@@ -0,0 +1,8 @@
+import Glassdor_Job_Posting_Scrapper as s
+import pandas as pd
+
+path = "C:/Users/Harshal/PycharmProjects/Job_Prediction_Project/chromedriver"
+
+df = s.get_jobs('data scientist',800, False, path, 20)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
